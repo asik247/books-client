10e1b91c64d8c214d79ff8b689f34c750f1128f8
Asikur Rahaman <mdasikur5893@gmail.com>
2026-04-15 23:35:46 +0600

    error solve

--- src/Components/Book.jsx
+++ src/Components/Book.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const Book = () => {
+const Book = ({ singleBook }) => {
+    // const { name, price } = singleBook;
+    console.log(singleBook);
+
     return (
-        <div>
-         <h4>single book</h4>   
+        <div style={{ border: "1px solid gray", margin: "10px", padding: "10px" }}>
+            <h4>{'name'}</h4>
+            <p>Price: {'price'}</p>
         </div>
     );
 };
--- src/Components/Books.jsx
+++ src/Components/Books.jsx
@@ -1,14 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import Book from './Book';
 
 const Books = () => {
-    useEffect(()=>{
+    const [allBooks, setAllBooks] = useState([]);
+
+    useEffect(() => {
         fetch("http://localhost:5000/books")
-        .then(res=>res.json())
-        .then(data=>console.log(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setAllBooks(data))
+            .catch(err => console.log(err));
+    }, []);
+
     return (
         <div>
             <h3>Books here</h3>
+
+            {allBooks.map(singleBook => (
+                <Book
+                    key={singleBook.id}
+                    singleBook={singleBook}
+                />
+            ))}
         </div>
     );
 };
