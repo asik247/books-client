aa95211f785fc1da0b4dad1b46d7184a167a9436
Asikur Rahaman <mdasikur5893@gmail.com>
2026-04-15 23:17:54 +0600

    server site data load books components

--- src/Components/Books.jsx
+++ src/Components/Books.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Books = () => {
+    useEffect(()=>{
+        fetch("http://localhost:5000/books")
+        .then(res=>res.json())
+        .then(data=>console.log(data))
+    },[])
     return (
         <div>
             <h3>Books here</h3>
