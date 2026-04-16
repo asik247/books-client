5c093870ef3bdcaeb0a74b4ed0e8a1f344e1835e
Asikur Rahaman <mdasikur5893@gmail.com>
2026-04-16 09:49:06 +0600

    handler add and post method code start now

--- src/Components/Books.jsx
+++ src/Components/Books.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import Book from './Book';
 const Books = () => {
+    const [allData, setAllData] = useState([])
+    useEffect(() => {
+        fetch("http://localhost:5000/boi")
+            .then(res => res.json())
+            .then(data => setAllData(data))
+    }, [])
+    // Handler
+    const handlerAdd = (e)=>{
+        e.preventDefault();
+        const name = e.target.name.value;
+        const price = e.target.price.value;
+        console.log('handler Add clicked',name,price);
+    }
     return (
         <div>
-            <h2>Books</h2>
+            <div className='font-bold text-2xl text-center mt-5'>
+                <h2>Add More Books!</h2>
+            </div>
+            {/* Input Field */}
+            <form onSubmit={handlerAdd} className='flex flex-col justify-center items-center'>
+                <input type="text" name="name" id="" placeholder='book name' />
+                <br />
+                <input type="text" name="price" id="" placeholder='book price' />
+                <br />
+                <button className='btn btn-primary'>Add</button>
+            </form>
+            <h2 className='text-center'>{allData.length}</h2>
+            {allData.map(singleData => (<div key={singleData.id} >
+                <div className='flex flex-col justify-center items-center'>
+                    <h1>{singleData.name}</h1>
+                </div>
+
+            </div>))}
         </div>
     );
 };
